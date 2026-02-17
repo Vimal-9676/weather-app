a6e288e2b6b9a5f2a18efc98f4ad8a4e56fbe37c
Vimal Yadav <vimal9676@gmail.com>
2026-02-17 21:48:17 +0530

    Updated for render deployment

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def index():
     return render_template("index.html", weather=weather_data, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
